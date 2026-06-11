3bdde5e72e33943456ea6776ab564ed152db1722
Shu Okabe <shurokabe@gmail.com>
2026-06-11 19:42:32 +0200

    Update utility functions.

--- code/bucc_style_dataset.py
+++ code/bucc_style_dataset.py
@@ -1,8 +1,31 @@
+import pandas as pd
 import random
 
 import utils as utils
 
 random.seed(42)
+
+
+# Pre-processing
+
+def remove_head_id(split_dataset):
+    '''Remove original sentence ID in the news dataset (Leipzig corpora).'''
+    new_split_dataset = [sentence.split('\t')[1] for sentence in split_dataset] 
+    return new_split_dataset
+
+def convert_split_parallel_into_df(source_file, target_file, src_lang, trg_lang):
+    '''Convert separate parallel data into data frame.'''
+    # Process the source file
+    source_list = source_file.split_file
+    # Process the target file
+    assert source_file.n_sent == target_file.n_sent, f'Not the same length: {source_file.n_sent} {target_file.n_sent}'
+    target_list = target_file.split_file
+
+    data_dict = {src_lang: [sentence.strip() for sentence in source_list], 
+                 trg_lang: [sentence.strip() for sentence in target_list]}
+    return pd.DataFrame.from_dict(data_dict)
+
+# Dataset creation
 
 def train_test_split(split_full_dataset):
     '''Split a full dataset into a training and test dataset.'''
@@ -12,7 +35,7 @@ def train_test_split(split_full_dataset):
     train_set = [sent.strip() for sent in split_full_dataset[:split_len]]
     test_set = [sent.strip() for sent in split_full_dataset[split_len:]]
     print(len(train_set), len(test_set))
-    return train_set, test_set #'\n'.join(train_set), '\n'.join(test_set)
+    return train_set, test_set
 
 def parallel_to_dictionary(source_sentence, target_sentence):
     '''Convert two lists of source and target parallel sentences into a dictionary.'''
@@ -22,12 +45,24 @@ def parallel_to_dictionary(source_sentence, target_sentence):
                            for i in range(n) if len(source_sentence[i]) > 2} 
     return parallel_dictionary
 
-def filter_parallel(source_list, target_list):
-    '''Filter parallel sentences to remove duplicated pairs.'''
-    n = len(source_list)
-    assert n == len(target_list), f'The two lists have different number of elements: {n} and {len(target_list)}.'
-    sentence_pairs = [(source_list[i], target_list[i]) for i in range(n)]
-    return utils.deduplicate_list(sentence_pairs) # set
+def subset_parallel_df(parallel_df, n_pair, seed=42):
+    '''Subsample from the parallel corpus in pandas DataFrame.'''
+    subset_df = parallel_df.sample(n=n_pair, random_state=seed)
+    print(subset_df.shape)
+    return subset_df
+
+def mono_n_sample(parallel_df, mono_src, mono_trg):
+    '''Get the number of samples for the monolingual corpus 
+    
+    Max: 19 times the size of the parallel dataset).
+    The number of TARGET sentences is 1.2 times that of the SOURCE sentences (maximum).'''
+    n_parallel = len(parallel_df)
+    n_sample_src = min(n_parallel * 19, len(mono_src)) # 19 instead of 20 to have 5%
+    print(f'{n_parallel} parallel sentences => {n_sample_src} monolingual SOURCE sentences')
+    n_sample_trg = min(int(n_sample_src * 1.2), len(mono_trg))
+    print(f'{n_sample_trg} monolingual TARGET sentences')
+    return n_sample_src, min(int(n_sample_src * 1.2), len(mono_trg))
+
 
 def create_dataset(monolingual_source, monolingual_target, parallel_source_list, parallel_target_list, seed=42):
     '''Create two monolingual datasets with injected parallel sentences.'''
@@ -36,12 +71,12 @@ def create_dataset(monolingual_source, monolingual_target, parallel_source_list,
     print(f'{len(split_mono_source)} monolingual source sentences.\n{len(split_mono_target)} monolingual target sentences.')
     assert not ('' in split_mono_source and '' in split_mono_target), 'Empty line in monolingual corpus list'
 
-    # Add parallel sentences
-    filter_parallel_list = filter_parallel(parallel_source_list, parallel_target_list)
-    filter_parallel_list = [sent_pair for sent_pair in filter_parallel_list 
-                            if (len(sent_pair[0]) > 2) and (len(sent_pair[1]) > 2)]
-    filter_para_source_list = [sent_pair[0] for sent_pair in filter_parallel_list]
-    filter_para_target_list = [sent_pair[1] for sent_pair in filter_parallel_list]
+    # # Add parallel sentences
+    # filter_parallel_list = filter_parallel(parallel_source_list, parallel_target_list)
+    # filter_parallel_list = [sent_pair for sent_pair in filter_parallel_list 
+    #                         if (len(sent_pair[0]) > 2) and (len(sent_pair[1]) > 2)]
+    filter_para_source_list = parallel_source_list # [sent_pair[0] for sent_pair in filter_parallel_list]
+    filter_para_target_list = parallel_target_list # [sent_pair[1] for sent_pair in filter_parallel_list]
     
     # Create parallel sentence dictionary
     parallel_dict = parallel_to_dictionary(filter_para_source_list, filter_para_target_list)
@@ -49,8 +84,9 @@ def create_dataset(monolingual_source, monolingual_target, parallel_source_list,
     split_mono_target.extend(filter_para_target_list)
     
     # Remove potential duplicated sentences
-    split_mono_source = list(set(split_mono_source))
-    split_mono_target = list(set(split_mono_target))
+    split_mono_source = utils.deduplicate_list(split_mono_source)
+    split_mono_target = utils.deduplicate_list(split_mono_target)
+    print('Same sentences?', split_mono_source[0], split_mono_target[0])
     print(f'Whole corpus:\n{len(split_mono_source)} monolingual source sentences.\n{len(split_mono_target)} monolingual target sentences.')
 
     # Shuffle both monolingual texts
@@ -69,9 +105,12 @@ def create_dataset(monolingual_source, monolingual_target, parallel_source_list,
     print(final_source_list[0], final_target_list[0])
     gold_list = [f'{pair[0]}\t{pair[1]}' for pair in gold_pair_list]
 
+    print(f'Source: {100 * (len(gold_list) / len(final_source_list)):.2f}%')
+    print(f'Target: {100 * (len(gold_list) / len(final_target_list)):.2f}%')
+
     return '\n'.join(final_source_list), '\n'.join(final_target_list), '\n'.join(gold_list)
 
-def split_shuffle_create_corpus(mono_src, mono_trg, para_src, para_trg):
+def split_shuffle_create_corpus(mono_src, mono_trg, para_src, para_trg, seed=42):
     '''Automatise train-test split, shuffling, and creation of BUCC-style corpus).
     
     Input is a split list.'''
@@ -89,10 +128,10 @@ def split_shuffle_create_corpus(mono_src, mono_trg, para_src, para_trg):
 
     # Create the datasets
     train_mono_src, train_mono_trg, train_gold_par = create_dataset(
-        train_split_mono_src, train_split_mono_trg, train_split_para_src, train_split_para_trg, seed=42)
+        train_split_mono_src, train_split_mono_trg, train_split_para_src, train_split_para_trg, seed=seed)
     
     test_mono_src, test_mono_trg, test_gold_par = create_dataset(
-        test_split_mono_src, test_split_mono_trg, test_split_para_src, test_split_para_trg, seed=42 + 1)
+        test_split_mono_src, test_split_mono_trg, test_split_para_src, test_split_para_trg, seed=seed + 1)
 
     return [[train_mono_src, train_mono_trg, train_gold_par], 
             [test_mono_src, test_mono_trg, test_gold_par]]
@@ -101,16 +140,12 @@ def save_files(src, trg, main_path, data_list):
     '''Save created BUCC-style files.
     
     data_list is the output of the split_shuffle_create_corpus function.'''
-    with open(f'{main_path}/{src}-{trg}.train.{src}', 'w') as f:
-        f.write(data_list[0][0])
-    with open(f'{main_path}/{src}-{trg}.train.{trg}', 'w') as f: 
-        f.write(data_list[0][1])
-    with open(f'{main_path}/{src}-{trg}.train.gold', 'w') as f: 
-        f.write(data_list[0][2])
+    # Train
+    utils.save_file(data_list[0][0], f'{main_path}/{src}-{trg}.train.{src}') # src
+    utils.save_file(data_list[0][1], f'{main_path}/{src}-{trg}.train.{trg}') # trg
+    utils.save_file(data_list[0][2], f'{main_path}/{src}-{trg}.train.gold') # gold
 
-    with open(f'{main_path}/{src}-{trg}.test.{src}', 'w') as f:
-        f.write(data_list[1][0])
-    with open(f'{main_path}/{src}-{trg}.test.{trg}', 'w') as f:
-        f.write(data_list[1][1])
-    with open(f'{main_path}/{src}-{trg}.test.gold', 'w') as f:
-        f.write(data_list[1][2]) 
+    # Test
+    utils.save_file(data_list[1][0], f'{main_path}/{src}-{trg}.test.{src}') # src
+    utils.save_file(data_list[1][1], f'{main_path}/{src}-{trg}.test.{trg}') # trg
+    utils.save_file(data_list[1][2], f'{main_path}/{src}-{trg}.test.gold') # gold
--- code/utils.py
+++ code/utils.py
@@ -1,14 +1,17 @@
 import re
 import unicodedata
 
+from sacremoses import MosesPunctNormalizer
 
+# Utility functions
 def delete_value_from_vector(vector, value):
     '''Delete a given value from a vector.
 
     To be used only when the value is in the vector.
     '''
     if value in vector:
-        vector.remove(value)
+        vector = [el for el in vector if el != value]
+        # vector.remove(value)
         return vector
     else:
         raise ValueError('The asked value is not in the vector.')
@@ -21,11 +24,20 @@ def text_to_line(raw_text, empty=True):
     else:
         return split_text
 
+def line_to_word(raw_line):
+    '''Split a sentence into a list of words (string) according to whitespace.'''
+    return re.split(' ', raw_line)
+
 def deduplicate_list(original_list):
     '''Remove duplicates from a list, while keeping the original order.'''
     return list(dict.fromkeys(original_list))
 
+def remove_excessive_whitespace(string):
+    '''Remove excessive whitespace.'''
+    return re.sub(' +', ' ', string)
 
+
+# Handling text files
 class Text:
     '''Basic processing of a text file.
     
@@ -47,9 +59,38 @@ class Text:
     '''
     def __init__(self, file_path, empty=True):
         self.raw_file = open(file_path, 'r').read()
-        normalised_text = unicodedata.normalize('NFC', self.raw_file)
-        self.pp_file = re.sub(' +', ' ', normalised_text)
+        self.pp_file = self.simple_preprocess()
 
         self.split_file = text_to_line(self.pp_file, empty=empty)
         self.n_sent = len(self.split_file)
-        print(f'There are {self.n_sent} sentences.')
+        unique_sent = set(self.split_file)
+        print(f'There are {self.n_sent} sentences ({len(unique_sent)} unique sentences).')
+    
+    def simple_preprocess(self):
+        '''Applying Unicode normalisation and spliting sentences.'''
+        normalised_text = unicodedata.normalize('NFC', self.raw_file)
+        # Punctuation normalisation
+        # normalised_text = punctuation_normalisation(normalised_text)
+        return simple_preprocessing(normalised_text)
+
+# Simple preprocessing
+def simple_preprocessing(text):
+    '''Apply preprocessing on the original text.'''
+    new_text = punctuation_normalisation(text)
+    new_text = remove_excessive_whitespace(new_text)
+    return new_text.strip()
+
+# Pre-processing punctuation
+mpn = MosesPunctNormalizer(lang='en')
+mpn.substitutions = [
+    (re.compile(r), sub) for r, sub in mpn.substitutions
+]
+
+def punctuation_normalisation(sent):
+    return mpn.normalize(sent).strip()
+
+# Save text file
+def save_file(text, path):
+    '''Save a text file in the desired path.'''
+    with open(path, 'w', encoding = 'utf8') as out_text:
+        out_text.write(text)
